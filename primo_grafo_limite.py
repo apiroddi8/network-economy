8f64ebbf4350f735538757f8a937bf2384c0fcd0
ali <eliss009@gmail.com>
2022-07-12 18:43:52 +0200

    3 domanda

--- primo_grafo_limite.py
+++ primo_grafo_limite.py
@@ -35,11 +35,9 @@ df_serie = pd.read_csv("Dataset/STEP4.csv")
 
 print(df['market_value'])
 df = df.astype({'market_value': float },errors='raise')
-first_quartile_market_value = df['market_value'].quantile(q=0.25)
-print(first_quartile_market_value)
 
 df_filtered = df[(df['country_team1'] == 'Italy') & (df['country_team2'] == 'Italy')]
-df_filtered = df_filtered[df_filtered['market_value'] >= first_quartile_market_value ]    #8000000.0 per fare grafo con poche squadre e milan juve centrali
+df_filtered = df_filtered[df_filtered['market_value'] >= 15000000.0 ]    #8000000.0 per fare grafo con poche squadre e milan juve centrali
 print(df_filtered)
 #
 G=nx.from_pandas_edgelist(df_filtered, "team1", "team2",create_using=nx.MultiDiGraph)
@@ -91,33 +89,14 @@ for i in G.nodes:
         if node[0] == i:
             in_degree = node[1]
     size_map.append(in_degree * 20)
-    for row in df_serie.itertuples():
-        if row.team1 == i:
-            if row.league_team1 == 'ITA1':
-                color_map.append('blue')
-            elif row.league_team1 == 'ITA2':
-                color_map.append('yellow')
-            elif row.league_team1 == 'ITA3':
-                color_map.append('green')
-            elif row.league_team1 == 'ITA4':
-                color_map.append('red')
-            else:
-                color_map.append('purple')
 
 
-legend_elements1 = [Line2D([0], [0], marker='o', color='w', label='Serie A',markerfacecolor='blue', markersize=13),
-                    Line2D([0], [0], marker='o', color='w', label='Serie B',markerfacecolor='yellow', markersize=13),
-                    Line2D([0], [0], marker='o', color='w', label='Serie C',markerfacecolor='green', markersize=13),
-                    Line2D([0], [0], marker='o', color='w', label='Serie D',markerfacecolor='red', markersize=13),
-                    Line2D([0], [0], marker='o', color='w', label='Primavera',markerfacecolor='purple', markersize=13)]
-
-pos = nx.kamada_kawai_layout(G)  # questa cosa l'ho presa dal report di Andrea Carta, dicono che i nodi che sono in posizione centrale sono quei nodi che sono maggiormente connessi con tutti gli altri
-# #                                         #nodi; mentre quelli nella periferia presentano il mimor numero di connessioni, e la loro distanza media (considerando il sentiero minimale) è alta.
+pos = nx.kamada_kawai_layout(G)
 
 nx.draw_networkx_nodes(G,pos,
                        nodelist=G.nodes,
                        node_size=size_map,
-                       node_color=color_map,
+                       node_color='blue',
                        alpha=0.7)
 nx.draw_networkx_edges(G,
                        pos=pos,
@@ -128,8 +107,8 @@ nx.draw_networkx_edges(G,
 nx.draw_networkx_labels(G, pos=pos,
                         labels=dict(zip(G.nodes,G.nodes)),
                         font_color='black',
-                        font_size=4)  # 7 per lo zoom
-ax.legend(handles=legend_elements1, loc='lower left', prop={'size': 8})
+                        font_size=5)  # 7 per lo zoom
+
 plt.show()
 
 #nx.write_gml(G, '../../Desktop/graph_1A.gml')
